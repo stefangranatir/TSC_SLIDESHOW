66fcae5f9998b87bf1798591ad9954e0c203800d
Štefan Granatir <116319250+stefangranatir@users.noreply.github.com>
2023-02-13 01:23:03 +0100

    Add files via upload

---
+++ TSC_SlideShowbyStefanG/TSC_SlideShowbyStefanG/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+
+
+namespace TSC_SlideShowbyStefanG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path = "ftp://192.168.1.206:8021";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Process.Start("explorer.exe", path);
+
+        }
+        string lokalna = "C:\\TSC_SlideShow";
+        string zipanje = "";
+
+        
+
+       
+    }
+}
